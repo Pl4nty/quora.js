f1aa3e082262f8fef7b46dd3600687ca8b53f27f
Pl4nty <admin@tplant.com.au>
2019-07-22 23:11:31 +1000

    OOP rewrite part 1
    Created base Page and Profile, will need to restructure files though and flesh out Page

--- src/profile.ts
+++ src/profile.ts
@@ -1,6 +1,11 @@
 const util = require('./util.js');
 
 /**
+ * profile.ts defines possible scraped stats extremely granularly to minimise unnecessary DOM parsing
+ * could possibly redesign if I can figure out how to run performance tests
+ */
+
+/**
  * Finds all valid credential pairs in a category
  * @param {CheerioInstance} $ - Cheerio instance with a profile page loaded
  * @param {String} listSelector - credential category's CSS selector
@@ -150,3 +155,75 @@ const profileStats = {
 
 //Indirect call to allow in-file testing
 module.exports = profileStats;
+
+class Page {
+    readonly uri: string;
+    protected $: CheerioStatic;
+
+    public lastRefreshed: Date;
+    
+    constructor (uri: string) {
+        this.uri = uri;
+        this.refresh();
+    }
+
+    public refresh() {
+        //implement getting page, parsing into cheerio (this.$) and error handling
+        got(this.uri)
+        
+        this.$ = cheerio.load("test");
+
+        this.lastRefreshed = new Date();
+    }
+}
+
+class Profile extends Page {
+    static uriScheme = /^test/
+
+    constructor (uri: string) {
+        super(uri)
+    }
+
+    // Meta properties
+    name = (): string => super.$('.user').eq(0).text();
+    anonymous = (): boolean => super.$('.anon_user').length !== 0;
+    verified = (): boolean => super.$('.VerificationBadge').length !== 0;
+    icon = (): string => super.$('.profile_photo_img').eq(0).attr('src');
+    defaultCredential = (): string => super.$('.UserCredential').eq(0).text();
+
+    // TODO remove commas from all numberic stats
+    // TODO add tests, especially for credentials and arrays. So many edge cases!
+    
+
+	// Not visible to anonymous users
+    // spaces = (): number => parseInt(super.$('.AnswersNavItem').find('.list_count').text()))
+    // Not visible to anonymous users
+    // shares = (): number => parseInt(super.$('.QuestionsNavItem').find('.list_count').text())
+    // Removed from UI, but may come back
+    // edits: (): number => parseInt(super.$('.OperationsNavItem').find('.list_count').text())  
+    
+    // Numeric stats
+    answers = (): number => parseInt(super.$('.AnswersNavItem').find('.list_count').text())
+    questions = (): number => parseInt(super.$('.QuestionsNavItem').find('.list_count').text())
+    posts = (): number => parseInt(super.$('.PostsNavItem').find('.list_count').text())
+    blogs = (): number => parseInt(super.$('.BlogsNavItem').find('.list_count').text())
+    followers = (): number => parseInt(super.$('.FollowersNavItem').find('.list_count').text())
+    following = (): number => parseInt(super.$('.FollowingNavItem').find('.list_count').text())
+    topics = (): number => parseInt(super.$('.TopicsNavItem').find('.list_count').text())
+    totalViews = (): number => parseInt(util.suffixedNumToInt(super.$('.AnswerViewsAboutListItem').find('.main_text').text().split(' ')[0]))
+    monthlyViews = (): number => parseInt(util.suffixedNumToInt(super.$('.AnswerViewsAboutListItem').find('.detail_text').text().split(' ')[0]))
+
+    // Category credentials
+    workCredential = (): number => parseInt(super.getStatList(super.$, '.WorkCredentialListItem'))
+    studyCredential = (): number => parseInt(super.getStatList(super.$, '.SchoolCredentialListItem'))
+    locationCredential = (): number => parseInt(super.getStatList($, '.LocationCredentialListItem'))
+
+    // Award stats
+    // TODO convert string[] years to numbers
+    topWriter = (): Array<string> => super.$('.TopWriterAboutListItem').find('.detail_text').text().replace(/\D/g,'').match(/.{1,4}/g)
+    topAsker = (): Array<string> => super.$('.TopAskerAboutListItem').find('.detail_text').text().replace(/\D/g,'').match(/.{1,4}/g)
+    // TODO finish these:
+    // sessionHost = (): Array<string> => 
+    // published = (): Array<string> => 
+    // knowledgePrize = (): number => parseInt(super.$('.datetime').text())
+}
