dfdd1d0f693a97784454b3fc7fd8d9af166a761d
Pl4nty <admin@tplant.com.au>
2019-08-07 23:05:30 +1000

    Finally fixed accessibility modifiers
    page almost finished, just graceful HTTP error handling to implement

--- src/models/page.ts
+++ src/models/page.ts
@@ -6,14 +6,16 @@ const version = require('../../package.json').version;
  * Parent Page class for all models
  */
 export default abstract class Page {
-    static quoraEndpoint = process.env.QUORA_ENDPOINT || "https://quora.com";
-    protected abstract urlFragment: string;
-    static uriScheme: RegExp;
-    static userAgent = process.env.QUORA_USERAGENT || "unspecifiedbot";
+    private static quoraEndpoint = process.env.QUORA_ENDPOINT || "https://quora.com";
+    private static userAgent = process.env.QUORA_USERAGENT || "quorajs";
 
-    protected readonly uri: string;
-    protected $: CheerioStatic;
+    private readonly uri: string;
     private lastRefreshed: Date;
+
+    protected $: CheerioStatic;
+
+    protected abstract urlFragment: string; //abstract to require in child models
+    protected static uriScheme: RegExp; //static as must be accessible from constructor
     
     constructor (uri: string) {
         if (uri.match(Page.uriScheme)) {
@@ -25,7 +27,8 @@ export default abstract class Page {
      * Reloads the page body into Cheerio
      */
     public async load(): Promise<void> {
-        //console.log(`trying to load ${this.urlFragment}${this.uri}`)
+        //console.log(`trying to load ${Page.quoraEndpoint}${this.urlFragment}${this.uri}`)
+        //TODO add more detailed error handling based on quora behaviour
         const res = await got(this.urlFragment + this.uri, {
             baseUrl: Page.quoraEndpoint,
             headers: {
@@ -40,5 +43,6 @@ export default abstract class Page {
         this.lastRefreshed = new Date();
     }
 
-    refreshDate = (): Date => this.lastRefreshed;
+    getUri = (): string => this.uri
+    getLastRefreshed = (): Date => this.lastRefreshed;
 }
